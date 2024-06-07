2b533b99ceed63a2f7ed679921516409869aaed5
@mad.s.mavlikhanov <s.mavlikhanov@madbrains.ru>
2024-06-08 00:14:11 +0400

    testing pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPwa from 'next-pwa'
+
+const nextConfig = withPwa({
+  dest: "public", disable: process.env.NODE_ENV === "development" ,
+});
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,27 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <link
+        rel="manifest"
+        href="/manifest.json"
+        crossOrigin="use-credentials"
+      />
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+      />
+      <link rel="apple-touch-icon" sizes="48x48" href="/icons/icon-48x48.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/icons/icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="256x256"
+        href="/icons/icon-256x256.png"
+      />
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
